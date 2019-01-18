838946e4b5b535a560f7da5786063bb3f5d9abe3
JoshDowling <jk2md@live.co.uk>
2019-01-18 11:35:35 +0000

    first domain commit

--- src/main/java/com/qa/persistence/domain/Classroom.java
+++ src/main/java/com/qa/persistence/domain/Classroom.java
@@ -1,26 +1,88 @@
 package com.qa.persistence.domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table (name = "Classroom" )
 public class Classroom {
 	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private Long classroomId;
+	
+	@Column(name = "Trainer_Name")
 	private String trainer;
-	private Class trainees;
 	
+	@Column(name = "trainees")
+	private Long traineeID;
 	
+	@OneToMany (mappedBy = "classroom")
+	@JoinColumn (name = "traineeID")
+	private List<Trainees> trainees;
 	
-	public Classroom(Long classroomId, String trainer, Class trainees) {
+		
+	public List<Trainees> getTrainees() {
+		return trainees;
+	}
+
+
+
+	public void setTrainees(List<Trainees> trainees) {
+		this.trainees = trainees;
+	}
+
+
+
+	public Classroom(Long classroomId, String trainer, Long traineeID) {
 		super();
 		this.classroomId = classroomId;
 		this.trainer = trainer;
-		this.trainees = trainees;
+		this.traineeID = traineeID;
+	}
+
+
+
+	public Long getClassroomId() {
+		return classroomId;
+	}
+
+
+
+	public void setClassroomId(Long classroomId) {
+		this.classroomId = classroomId;
+	}
+
+
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+
+
+	public Long getTraineeID() {
+		return traineeID;
+	}
+
+
+
+	public void setTraineeID(Long traineeID) {
+		this.traineeID = traineeID;
 	}
 	
 	
---
+++ src/main/java/com/qa/persistence/domain/Trainees.java
@@ -0,0 +1,66 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Trainees {
+	
+
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Id
+		private Long traineeID;
+		
+		@Column(name = "Trainee_Name")
+		private String traineeName;
+		
+		@ManyToOne(fetch = FetchType.LAZY)
+		private Classroom classroom;
+		
+		
+		public Classroom getClassroom() {
+			return classroom;
+		}
+
+
+		public void setClassroom(Classroom classroom) {
+			this.classroom = classroom;
+		}
+
+
+		public void Classroom(Long traineeID, String traineeName) {
+			this.traineeID = traineeID;
+			this.traineeName = traineeName;
+		}
+
+
+
+		public Long getTraineeID() {
+			return traineeID;
+		}
+
+
+
+		public void setTraineeID(Long traineeID) {
+			this.traineeID = traineeID;
+		}
+
+
+
+		public String getTraineeName() {
+			return traineeName;
+		}
+
+
+
+		public void setTraineeName(String traineeName) {
+			this.traineeName = traineeName;
+		}
+	
+	
+}
